d34495b27083832f90adfb1797b757b3a2a20887
nikolay-egorov <deathadder98@gmail.com>
2019-04-19 19:48:37 +0300

    client routing building

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import routes from '@/routes'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  // routes: [
+  //   {
+  //     path: '/',
+  //     name: 'HelloWorld',
+  //     component: HelloWorld
+  //   }
+  // ]
+  mode: 'history',
+  routes
 })
---
+++ client/src/routes/index.js
@@ -0,0 +1,17 @@
+//client routes here
+
+import Start from '@/components/pages/StartPage'
+import Posts from '@/components/pages/PostsPage'
+const routes = [
+  {
+    path: '/',
+    name: 'Start',
+    component: Start
+  },
+  {
+    path: '/posts',
+    name: 'Posts',
+    component: Posts
+  }
+]
+export default routes
